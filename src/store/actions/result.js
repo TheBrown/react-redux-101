4a137714d087ae7cce486ddd8c9d73cf2a6774e0
Saleumsack <saleumsack16@gmail.com>
2018-10-13 23:41:36 +0700

    Refactoring Actions

---
+++ src/store/actions/result.js
@@ -0,0 +1,21 @@
+import * as actionTypes from './actionsTypes';
+export const saveResult = res => {
+    return  {
+        type: actionTypes.STORE_RESULT,
+        result: res
+    };
+}
+
+export const storeResult = (result) => {
+    return dispatch => {setTimeout(() => {
+        dispatch(saveResult(result));
+    }, 2000);
+}
+}
+
+export const deleteResult = (resultid) => {
+    return {
+        type: actionTypes.DELETE_RESULT,
+        resultElId: resultid
+    };
+}
